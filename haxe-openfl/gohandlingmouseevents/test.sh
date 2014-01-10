08a1dd2d6f4c0ccc2c6459838bf5aecdbd345016
elliott5 <elliott@elliott-stonehams-macbook-pro.local>
2014-01-10 17:18:48 +0000

    Add hello world example, update openfl example

--- haxe-openfl/gohandlingmouseevents/test.sh
+++
@@ -1,7 +0,0 @@
-# only tested on OSX, requires tgo compiler
-cd Source/tardis
-tgo main.go
-if [ "$?" = "0" ]; then
-	cd ../..
-	lime test $*
-fi
---
+++ haxe/helloworld/helloworld.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	println("Hello world! 你好世界！ ") // NOTE fmt.Printf does not work as at January 2014
+}
