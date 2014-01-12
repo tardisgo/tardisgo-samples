fa8e7fa9e9ed724f1028570ca90ba875f8c80c5d
elliott5 <elliott@elliott-stonehams-macbook-pro.local>
2014-01-12 08:31:22 +0000

    Remove colon in comment

--- haxe/helloworld/helloworld.go
+++ haxe/helloworld/helloworld.go
@@ -1,7 +1,7 @@
-﻿// example for tardisgo.github.io
+// example for tardisgo.github.io
 package main
 
 func main() {
 	println("Hello World!") // NOTE fmt.Printf does not work in TARDIS Go as at Jan'14
-	println("你好世界！") // NOTE: Chinese version may print incorrectly on Windows hosts (like golang proper) or when targeting Java. 
+	println("你好世界！")        // NOTE Chinese version may print incorrectly on Windows hosts (like golang proper) or when targeting Java.
 }
