5294bf16b8239cf3cb4215cd75d6640937b2c48d
Elliott Stoneham <elliott.stoneham@gmail.com>
2014-01-28 11:51:15 +0000

    Add 8 gopher animation for FOSDEM14

---
+++ openfl/8concurrentgophers/Source/Main.hx
@@ -0,0 +1,229 @@
+package; // written for FOSDEM14
+
+import flash.display.Bitmap;
+import flash.display.Sprite;
+import flash.events.Event;
+import flash.events.MouseEvent;
+import flash.utils.Timer;
+import openfl.Assets;
+import flash.text.TextField;
+import flash.text.TextFieldAutoSize;
+
+import tardis.Go; // import of the Go code
+
+
+class Main extends Sprite {
+	
+	private var bigpile:Array<Sprite>;
+	private var oven:Array<Sprite>;
+	private var Code:Sprite;
+	private var Logo1:Sprite;
+	private var Logo2:Sprite;
+	
+	private var Sprite1:Array<Sprite>;
+	private var s1x = 90;
+	private var s1y = 45;
+	
+	private var Sprite2:Array<Sprite>;
+	private var s2x = 420;
+	private var s2y = 45;
+	
+	private var Books:Array<Sprite>;
+	private var bx = 390;
+	private var by = 50; 
+	
+	private var lineOffsets:Array<Int>;
+	
+	private var goTimer:Timer;
+	
+	private var textField:TextField;
+	private var whatToDo:TextField;
+	private var headline:TextField;
+
+	public function new () {
+		
+		super ();
+
+		// headline at the top
+		headline = new TextField ();
+		headline.selectable = true;
+		headline.x = 300;
+		headline.y = 10;
+		headline.width = 300;
+		headline.text = "TARDIS Go example; see tardisgo.github.io";
+		headline.textColor = 0x008000;		
+		addChild (headline);
+
+		// the code extract in the centre
+		Code = new Sprite ();
+		Code.addChild (new Bitmap (Assets.getBitmapData ("assets/function.png")));
+		Code.x = 200;
+		Code.y = 110;
+		addChild (Code);
+		
+		// the left hand code indicator
+		Logo1 = new Sprite ();
+		Logo1.addChild (new Bitmap (Assets.getBitmapData ("assets/gophercolor16x16.png")));
+		Logo1.x = 230;
+		Logo1.y = 140;
+		addChild (Logo1);
+
+		// the right hand code indicator
+		Logo2 = new Sprite ();
+		Logo2.addChild (new Bitmap (Assets.getBitmapData ("assets/gophercolor16x16flipped.png")));
+		Logo2.x = 540;
+		Logo2.y = 140;
+		addChild (Logo2);
+	
+		// Explation text on the left
+		whatToDo = new TextField ();
+		whatToDo.selectable = false;
+		whatToDo.x = 10;
+		whatToDo.y = 130;
+		whatToDo.width = 200;
+		whatToDo.height = 200;
+		whatToDo.text = "All animated gophers are \nrunning the Go code on the right.\nThe logos show where the 2 above\ngophers each are in that code now.\nThis Go code is running live,\ntranspiled into: "+Go.Platform();
+		whatToDo.textColor = 0x008000;		
+		addChild (whatToDo);
+
+		bigpile = [null,null,null,null];
+		oven = [null,null,null,null];		
+		Books = [null,null,null,null];
+		Sprite1 = [null,null,null,null];		
+		Sprite2 = [null,null,null,null];
+		lineOffsets = [0,210,305,400];
+			
+		for (l in 0...4){
+			// big pile of books on the left
+			bigpile[l] = new Sprite ();
+			bigpile[l].addChild (new Bitmap (Assets.getBitmapData ("assets/bigpile.png")));
+			bigpile[l].x = 10;
+			bigpile[l].y = 20+lineOffsets[l];
+			addChild(bigpile[l]);
+
+			// oven on the right
+			oven[l] = new Sprite ();
+			oven[l].addChild (new Bitmap (Assets.getBitmapData ("assets/oven.png")));
+			oven[l].x = 690;
+			oven[l].y = 0+lineOffsets[l];
+			addChild(oven[l]);
+		
+			// books (sometimes) in the middle
+			Books[l] = new Sprite ();
+			Books[l].addChild (new Bitmap (Assets.getBitmapData ("assets/smallpile.png")));
+			Books[l].x = bx;
+			Books[l].y = by+lineOffsets[l];
+			addChild(Books[l]);
+		
+			// the left hand gopher
+			Sprite1[l] = new Sprite ();
+			Sprite1[l].addChild ( new Bitmap (Assets.getBitmapData ("assets/pick.png")));
+			Sprite1[l].x = s1x;
+			Sprite1[l].y = s1y+lineOffsets[l];
+			addChild(Sprite1[l]);
+			
+			// the right hand gopher
+			Sprite2[l] = new Sprite ();
+			Sprite2[l].addChild ( new Bitmap (Assets.getBitmapData ("assets/pick.png")));
+			Sprite2[l].x = s2x;
+			Sprite2[l].y = s2y+lineOffsets[l];
+			addChild(Sprite2[l]);
+		}
+					
+		// the "inspired by"" text
+		textField = new TextField ();
+		textField.selectable = true;
+		textField.x = 420;
+		textField.y = 195;
+		textField.width = 400;
+		textField.height = 50;
+		textField.textColor = 0x008000;		
+		textField.text = "Inspired by \"Concurrency is not Parallelism (it's better)\" - Rob Pike\nhttp://concur.rspace.googlecode.com/hg/talk/concur.html";
+		addChild (textField);
+		
+		// off we go...
+		s1laststatus = [0, 0, 0];
+		s2laststatus = [0, 0, 0];
+		bookslaststatus = [true, true, true];
+		Go.init(); // initialise the Go environment
+		Go_main_StartGophers.callFromHaxe(); // start the gopher animation goroutines
+		goTimer=new Timer(25); // 25 ms per run
+		goTimer.addEventListener ( "timer", goTimerEvent); // schedule some go every so often
+		goTimer.start();
+	}
+	
+
+	private var s1laststatus : Array<Int> ;
+	private var s2laststatus : Array<Int> ;
+	private var bookslaststatus : Array<Bool> ;
+
+	private function goTimerEvent(e:Dynamic):Void { // the entry point for every tick
+		
+		Scheduler.timerEventHandler(e); // schedule the Go code
+
+		for(l in 0...4){
+
+			if(Go.main_smallpiles.load()[l].hasContents()!=bookslaststatus[l]){
+				bookslaststatus[l]=Go.main_smallpiles.load()[l].hasContents();
+				Books[l].removeChildAt(0);		
+				if(Go.main_smallpiles.load()[l].hasContents()==true){		
+					Books[l].addChild (new Bitmap (Assets.getBitmapData ("assets/smallpile.png")));			
+				} else {
+					Books[l].addChild (new Bitmap (Assets.getBitmapData ("assets/emptypile.png")));			
+				}
+			}
+		
+			if((Go.main_Sprite1state.load()[l]|0)!=s1laststatus[l]){
+			
+				// make sure we don't leave any stray pixels unset and flag for GC
+				Sprite1[l].removeChildAt(0);
+	
+				s1laststatus[l] = Go.main_Sprite1state.load()[l]|0; // switch to the new state
+				if(l==0)
+					Logo1.y = 140 + (15*s1laststatus[l]); // move the logo to reflect the new state
+			
+				// add the new image
+				if(s1laststatus[l]==Go.main_Pick) {
+					Sprite1[l].addChild (new Bitmap (Assets.getBitmapData ("assets/pick.png")));
+				}
+				if(s1laststatus[l]==Go.main_Full) {
+					Sprite1[l].addChild (new Bitmap (Assets.getBitmapData ("assets/full.png")));
+				}
+				if(s1laststatus[l]==Go.main_Shovel) {
+					Sprite1[l].addChild (new Bitmap (Assets.getBitmapData ("assets/shovel.png")));
+				}
+				if(s1laststatus[l]==Go.main_Empty) {
+					Sprite1[l].addChild (new Bitmap (Assets.getBitmapData ("assets/empty.png")));
+				}
+			}
+			Sprite1[l].x = s1x + Go.main_Sprite1X.load()[l];
+			Sprite1[l].y = s1y + Go.main_Sprite1Y.load()[l] + lineOffsets[l];
+			
+			if((Go.main_Sprite2state.load()[l]|0)!=s2laststatus[l]){
+				
+				// make sure we don't leave any stray pixels unset and flag for GC
+				Sprite2[l].removeChildAt(0);
+				
+				s2laststatus[l] = Go.main_Sprite2state.load()[l]|0; // switch to the new state
+				if (l==0)
+						Logo2.y = 140 + (15*s2laststatus[l]); // move the logo to reflect the new state
+				
+				// add the new image
+				if(s2laststatus[l]==Go.main_Pick) {
+					Sprite2[l].addChild (new Bitmap (Assets.getBitmapData ("assets/pick.png")));
+				}
+				if(s2laststatus[l]==Go.main_Full) {
+					Sprite2[l].addChild (new Bitmap (Assets.getBitmapData ("assets/full.png")));
+				}
+				if(s2laststatus[l]==Go.main_Shovel) {
+					Sprite2[l].addChild (new Bitmap (Assets.getBitmapData ("assets/shovel.png")));
+				}
+				if(s2laststatus[l]==Go.main_Empty) {
+					Sprite2[l].addChild (new Bitmap (Assets.getBitmapData ("assets/empty.png")));
+				}
+			}
+			Sprite2[l].x = s2x + Go.main_Sprite2X.load()[l];
+			Sprite2[l].y = s2y + Go.main_Sprite2Y.load()[l] + lineOffsets[l];
+		}			
+	}
+}
---
+++ openfl/8concurrentgophers/Source/gophers.go
@@ -0,0 +1,124 @@
+// animation inspired by http://concur.rspace.googlecode.com/hg/talk/concur.html#landing-slide
+// gopher logo by Renée French
+
+// this version, with 8 animated gophers, made for FOSDEM'14 presentation
+
+package main
+
+import "github.com/tardisgo/tardisgo/tardisgolib"
+
+func main() {}
+
+const numLines = 4
+
+// the globals below are inspected by the Haxe code to move and change sprites to create the animation
+var Sprite1X, Sprite1Y, Sprite2X, Sprite2Y [numLines]float64
+var Sprite1state, Sprite2state [numLines]int
+var bigpiles, smallpiles, ovens [numLines]chan int
+
+const ( // constants for the state of a gopher, also used by Haxe code
+	Pick = iota
+	Full
+	Shovel
+	Empty
+)
+
+// This function is called to set-off the gophers
+func StartGophers() {
+	for l := 0; l < numLines; l++ {
+		bigpiles[l] = make(chan int)
+		bigpiles[l] <- 1 * (l + 1)  // start low, so that left-hand gopher moves fast
+		go fillbigpile(bigpiles[l]) // keep adding randomly to the big pile
+		smallpiles[l] = make(chan int)
+		smallpiles[l] <- 10 / (l + 1) // start high, so that right-hand gopher moves slow
+		ovens[l] = make(chan int)
+		go fire(ovens[l]) // burn everything that arrives!
+
+		// now start off the two top gophers
+		go gopher(&Sprite1X[l], &Sprite1Y[l], &Sprite1state[l], bigpiles[l], smallpiles[l])
+		go gopher(&Sprite2X[l], &Sprite2Y[l], &Sprite2state[l], smallpiles[l], ovens[l])
+	}
+}
+
+func fillbigpile(bigpile chan int) {
+	for {
+		select { // randomized select to create large or small loads
+		case bigpile <- 1:
+		case bigpile <- 2:
+		case bigpile <- 3:
+		case bigpile <- 8:
+		case bigpile <- 9:
+		case bigpile <- 10:
+		}
+	}
+}
+
+func fire(oven chan int) {
+	for {
+		<-oven
+	}
+}
+
+// an individual gopher, animated with logos by the Haxe code
+func gopher(x, y *float64, state *int, in, out chan int) {
+	for {
+		cartLoad := pickBooks(x, y, state, in)
+		pushBooks(x, y, state, cartLoad)
+		fireBooks(x, y, state, cartLoad, out)
+		moreBooks(x, y, state)
+	}
+}
+
+func pickBooks(x, y *float64, state *int, in chan int) int {
+	*state = Pick
+	*x = 0
+	v := <-in
+	loop(v) // spend longer picking some loads and putting them on the cart
+	return v
+}
+
+func pushBooks(x, y *float64, state *int, cartLoad int) {
+	*state = Full
+	for *x = 0.0; *x < 150.0; (*x) += 10.0 / float64(cartLoad) {
+		if *y > 0.0 { // create bumps in the road
+			*y = 0.0
+		} else {
+			*y = float64(tardisgolib.HAXE("Std.random(3);")) // random small bumps
+		}
+		tardisgolib.Gosched() // without this, the animation would not show each state
+	}
+	if *x > 150.0 { // constrain large x offsets
+		*x = 150.0
+	}
+	*y = 0.0
+}
+
+func fireBooks(x, y *float64, state *int, cartLoad int, out chan int) {
+	*state = Shovel
+	loop(cartLoad) // spend longer unloading some loads into the fire
+	out <- cartLoad
+}
+
+func moreBooks(x, y *float64, state *int) {
+	*state = Empty
+	for *x > 0.0 {
+		*x -= 10.0
+		if *x < 0.0 { // no -ve x offsets please
+			*x = 0.0
+		}
+		if *y > 0.0 { // create bumps in the road
+			*y = 0.0
+		} else {
+			*y = float64(tardisgolib.HAXE("Std.random(5);")) // random bigger bumps
+		}
+		tardisgolib.Gosched() // would not show state without this, the animation would jump.
+	}
+	*y = 0.0
+}
+
+func loop(n int) { // add some delay when required
+	for n > 0 {
+		n--
+		tardisgolib.Gosched() // give up control in order to show the gopher waiting
+	}
+}
