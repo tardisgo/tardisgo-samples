3061491c32fc5e3242fd91350662f52f07086d74
Elliott Stoneham <elliott.stoneham@gmail.com>
2015-01-23 16:27:07 +0000

    update tgoall.sh to use new memory flag

--- scripts/tgoall.sh
+++ scripts/tgoall.sh
@@ -3,10 +3,10 @@
 # NOTE neko vm only works on very small examples 
 tardisgo $* *.go
 if [ "$?" = "0" ]; then
-	haxe -main tardis.Go -dce full -swf tardis/tardisgo.swf
+	haxe -main tardis.Go -dce full -swf tardis/go.swf
 	#haxe -main tardis.Go -dce full -neko tardis/tardisgo.n
-	haxe -main tardis.Go -dce full -D dataview -js tardis/tardisgo_dv.js
-	haxe -main tardis.Go -dce full -js tardis/tardisgo.js
+	haxe -main tardis.Go -dce full -D fullunsafe -js tardis/go_fu.js
+	haxe -main tardis.Go -dce full -js tardis/go.js
 	haxe -main tardis.Go -dce full -cpp tardis/cpp 
 	haxe -main tardis.Go -dce full -java tardis/java
 	haxe -main tardis.Go -dce full -cs tardis/cs
@@ -15,12 +15,12 @@ if [ "$?" = "0" ]; then
 	haxe -main tardis.Go --interp
 	#echo "Neko (only works on very small projects):"
 	#neko tardis/tardisgo.n
-	echo "Node/JS (using dataview):"
+	echo "Node/JS (using fullunsafe):"
 	# NOTE on Linux, the line below should use nodejs rather than node
-	node < tardis/tardisgo_dv.js
+	node < tardis/go_fu.js
 	echo "Node/JS:"
 	# NOTE on Linux, the line below should use nodejs rather than node
-	node < tardis/tardisgo.js
+	node < tardis/go.js
 	echo "CPP:"
 	./tardis/cpp/Go
 	echo "Java:"
@@ -30,5 +30,5 @@ if [ "$?" = "0" ]; then
 	echo "PHP:"
 	php tardis/php/index.php
 	echo "Opening swf file (Chrome as a file association for swf works to test on OSX):"
-	open tardis/tardisgo.swf
+	open tardis/go.swf
 fi
