53b3818380c7f386824daa2cbed50c5551ded6d6
Elliott Stoneham <elliott.stoneham@gmail.com>
2015-04-20 11:58:52 +0100

    testing potential for hxml

---
+++ wip/parsefloat/all.sh
@@ -0,0 +1,4 @@
+haxe -main tardis.Go -cp tardis -dce full -js tardis/go.js
+haxe -main tardis.Go -cp tardis -dce full -cs tardis/cs
+haxe -main tardis.Go -cp tardis -dce full -java tardis/java
+haxe -main tardis.Go -cp tardis -dce full -cpp tardis/cpp
