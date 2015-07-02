52c3b59f93e7f647fc21609ea47b90cbd5dda98d
Elliott Stoneham <elliott.stoneham@gmail.com>
2015-07-02 09:03:59 +0100

    Add benchmark figs for JS after Closure Compiler

--- benchmarks/bench.sh
+++ benchmarks/bench.sh
@@ -7,32 +7,50 @@ echo "go:"
 time ./mandel
 echo "TARDISgo default settings"
 tardisgo -haxe all mandel.go
-echo "TARDISgo benchmark settings"
-tardisgo -haxe bench mandel.go
+java -jar ~/closure/compiler.jar  --js tardis/go.js >tardis/go-opt.js 2>/dev/null
+echo "JS after closure"
+time node<tardis/go-opt.js
+#echo "TARDISgo benchmark settings"
+#tardisgo -haxe bench mandel.go
+#java -jar ~/closure/compiler.jar  --js tardis/go-bench.js >tardis/go-bench-opt.js 2>/dev/null
+#echo "benchmark settings JS after closure"
+#time node<tardis/go-bench-opt.js
 echo "GopherJS"
 gopherjs build mandel.go
 time node<mandel.js
 cd ../fannkuch
 echo "fannkuch benchmark:"
 go build fannkuch.go
-echo "go:"
+echo "fannkuch go:"
 time ./fannkuch
-echo "TARDISgo default settings"
+echo "fannkuch TARDISgo default settings"
 tardisgo -haxe all fannkuch.go
-echo "TARDISgo benchmark settings"
-tardisgo -haxe bench fannkuch.go
-echo "GopherJS"
+java -jar ~/closure/compiler.jar  --js tardis/go.js >tardis/go-opt.js 2>/dev/null
+echo "fannkuch JS after closure"
+time node<tardis/go-opt.js
+#echo "fannkuch TARDISgo benchmark settings"
+#tardisgo -haxe bench fannkuch.go
+#java -jar ~/closure/compiler.jar  --js tardis/go-bench.js >tardis/go-bench-opt.js 2>/dev/null
+#echo "fannkuch benchmark settings JS after closure"
+#time node<tardis/go-bench-opt.js
+echo "fannkuch GopherJS"
 gopherjs build fannkuch.go
 time node<fannkuch.js
 cd ../binarytree
 echo "binarytree benchmark:"
 go build binarytree.go
-echo "go:"
+echo "binarytree go:"
 time ./binarytree
-echo "TARDISgo default settings"
+echo "binarytree TARDISgo default settings"
 tardisgo -haxe all binarytree.go
-echo "TARDISgo benchmark settings"
-tardisgo -haxe bench binarytree.go
-echo "GopherJS"
+java -jar ~/closure/compiler.jar  --js tardis/go.js >tardis/go-opt.js 2>/dev/null
+echo "binarytree JS after closure"
+time node<tardis/go-opt.js
+#echo "binarytree TARDISgo benchmark settings"
+#tardisgo -haxe bench binarytree.go
+#java -jar ~/closure/compiler.jar  --js tardis/go-bench.js >tardis/go-bench-opt.js 2>/dev/null
+#echo "binarytree benchmark settings JS after closure"
+#time node<tardis/go-bench-opt.js
+echo "binarytree GopherJS"
 gopherjs build binarytree.go
 time node<binarytree.js
---
+++ wip/gocallingcpphaxe/examplemain.cpp
@@ -0,0 +1,65 @@
+/*
+ This is an example mainline that can be used to link a static version.
+ First you need to build the static version of the standard libs, with:
+ cd $HXCPP/runtime
+ haxelib run hxcpp BuildLibs.xml -Dstatic_link
+
+ Then the static verion of your application with (note: extra space before 'static_link'):
+
+ haxe -main YourMain -cpp cpp -D static_link
+
+ You then need to link the above libraries with this (or a modified version) main.
+ You may choose to create a VisualStudio project, and add the libraries from
+  $HXCPP/bin/Windows/(std,regexp,zlib).lib and your application library.
+ 
+  Note also, that if you compile with the -debug flag, your library will have a different name.
+
+  Linking from the command line for windows (user32.lib only required for debug version):
+
+  cl ExampleMain.cpp cpp/YourMain.lib $HXCPP/bin/Windows/std.lib $HXCPP/bin/Windows/zlib.lib  $HXCPP/bin/Windows/regexp.lib user32.lib
+
+  From other OSs, the compile+link command will be different.  Here is one for mac:
+
+   g++ ExampleMain.cpp cpp/Test-debug.a $HXCPP/bin/Mac/regexp.a $HXCPP/bin/Mac/std.a $HXCPP/bin/Mac/zlib.a
+
+  If you wish to add other static libraries besides these 3 (eg, nme) you will
+   need to compile these with the "-Dstatic_link" flag too, and call their "register_prims"
+   init call.  The inclusion of the extra static library will require the library
+   in the link line, and may requires additional dependencies to be linked.
+   Also note, that there may be licensing implications with static linking
+   thirdparty libraries.
+
+*/
+
+#include <stdio.h>
+
+/*
+extern "C" const char *hxRunLibrary();
+extern "C" void hxcpp_set_top_of_stack();
+*/	
+
+// Declare additional library entry points...
+//extern "C" int nme_register_prims();
+
+extern "C" int hxmain(int argc, char *argv[])	
+{
+	// Do this first
+  printf("hxmain!\n");
+	/*
+  hxcpp_set_top_of_stack();
+  */
+   // Register additional ndll libaries ...
+   // nme_register_prims();
+
+	printf("Begin!\n");
+ 	/*
+  const char *err = hxRunLibrary();
+	if (err) {
+		// Unhandled exceptions ...
+		fprintf(stderr,"Error %s\n", err );
+		return -1;
+	}
+  */
+	printf("Done!\n");
+	return 0;
+}
